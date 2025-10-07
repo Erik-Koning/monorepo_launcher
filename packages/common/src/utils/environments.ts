2b066e3897be7093dc03452ce1068c39585bd704
erik koning <erik@koning.ca>
2025-10-07 10:05:06 -0400

    .env example, build success, still dynamic errors from root layout.tsx

--- packages/common/src/utils/environments.ts
+++ packages/common/src/utils/environments.ts
@@ -2,25 +2,25 @@ import { emailIsSuperAppAdmin } from "@common/utils/email";
 
 export const isProdEnv = () => {
   return process.env.NEXT_PUBLIC_NODE_ENV
-    ? process.env.NEXT_PUBLIC_NODE_ENV.startsWith("prod")
+    ? process.env.NEXT_PUBLIC_NODE_ENV?.startsWith("prod")
     : process.env.NODE_ENV
-    ? process.env.NODE_ENV.startsWith("prod")
+    ? process.env.NODE_ENV?.startsWith("prod")
     : false;
 };
 
 export const isTestEnv = () => {
   return process.env.NEXT_PUBLIC_NODE_ENV
-    ? process.env.NEXT_PUBLIC_NODE_ENV.startsWith("test")
+    ? process.env.NEXT_PUBLIC_NODE_ENV?.startsWith("test")
     : process.env.NODE_ENV
-    ? process.env.NODE_ENV.startsWith("test")
+    ? process.env.NODE_ENV?.startsWith("test")
     : false;
 };
 
 export const isDevEnv = () => {
   return process.env.NEXT_PUBLIC_NODE_ENV
-    ? process.env.NEXT_PUBLIC_NODE_ENV.startsWith("dev")
+    ? process.env.NEXT_PUBLIC_NODE_ENV?.startsWith("dev")
     : process.env.NODE_ENV
-    ? process.env.NODE_ENV.startsWith("dev")
+    ? process.env.NODE_ENV?.startsWith("dev")
     : false;
 };
 
